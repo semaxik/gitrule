ff250d828a0e9acadd54b453b305c27fabe54545
semaxik <semaxik@outlook.com>
2025-05-18 11:45:10 +0300

    rewritten finctions Add friend and add enemy

--- Animalsreserve/Animals reserve/Animals reserve.cpp
+++ Animalsreserve/Animals reserve/Animals reserve.cpp
@@ -3,10 +3,9 @@
 #include "dog.hpp"
 #include "cat.hpp"
 int main() {
-	Dog bobik("bobic");
+	Dog bobik("bobik");
 	Dog shah("shah");
 	Dog barbos("barbos");
-
 	bobik.add_enemy(shah);
 	if (bobik.has_enemy(shah)) {
 		std::cout << "гулять нельзя, враг!!!"<<std::endl;
--- Animalsreserve/Animals reserve/Pet.hpp
+++ Animalsreserve/Animals reserve/Pet.hpp
@@ -1,28 +1,66 @@
 #pragma once
 #include <iostream>
+#include <vector>
 class Pet {
 private:
 	int enemy_count = 0;
-	Pet enemyes[10];
+	Pet enemyes1[];
 	int friend_count = 0;
-	Pet friends[10];
+	Pet friends1[];
 
 public:
 	virtual void feed(int hours, int minutes) = 0;
 	virtual void sleep(int hours, int minutes) = 0;
 	virtual void walk(int hours, int minutes) = 0;
 	virtual void friends(char friends, char enemyes) = 0;
-	virtual void add_enemy(const Pet& enemy) {
-		enemyes[enemy_count] = enemy;
-		++enemy_count;
-		if (enemy_count > 10) {
-			std::cout << "Переполнение врагов" << std::endl;
+	virtual void add_enemy(const Pet& enemy, Pet pet, int num) {
+		//Задание N1
+		template <typename Pet>;
+		std::vector<Pet> enemies;
+		enemies.push_back(pet);
+		//Задание N2
+		for (int i = 0; i < enemies; ++i) {
+			std::cin >> enemyes1{i} >> std::endl;
+		}
+		std::cin >> enemies >> std::endl;
+		for (int i = 0; i < enemies; ++i) {
+			std::cout << enemies << std::endl;
+		}
+		for (auto& number : enemies) {
+			std::cout << number << std::endl;
+		}
+		//Задание N3
+		for (int i = 0; i < enemies; ++i) {
+			std::cin >> num >> std::endl;
+			if (enemies[i] == num) {
+				std::cout << "Найдено" << std::endl;
+			}
+			else {
+				std::cout << "Не найдено" << std::endl;
+			}
 		}
 	}
-	virtual void add_friend(const Pet& friends) {
-		friends[friend_count] = friends;
-		++friend_count;
-		if (friend_count > 10) {
-			std::cout << "Переполнение друзей" << std::endl;
-	}
+	virtual void add_friend(const Pet& friends, Pet pet, int num) {
+		//Задание N1
+		template <typename Pet>;
+		std::vector<Pet> friends;
+		friends.push_back(pet);
+		//Задание N2
+		for (int i = 0; i < enemies; ++i) {
+			std::cin >> enemyes1{ i } >> std::endl;
+		}
+		std::cin >> enemies >> std::endl;
+		for (auto& number : enemies) {
+			std::cout << number << std::endl;
+		}
+		//Задание N3
+		for (int i = 0; i < enemies; ++i) {
+			std::cin >> num >> std::endl;
+			if (enemies[i] == num) {
+				std::cout << "Найдено" << std::endl;
+			}
+			else {
+				std::cout << "Не найдено" << std::endl;
+			}
+		}
 };
