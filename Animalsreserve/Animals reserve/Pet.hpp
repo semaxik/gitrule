4cae33591187450433e07b6b11d4cf3338a7b205
Alexandr Petrov <alexandr.petrov@innroad.com>
2025-06-03 21:35:22 +0300

    replaced pet with petId for enemies and friends lists

--- Animalsreserve/Animals reserve/Pet.hpp
+++ Animalsreserve/Animals reserve/Pet.hpp
@@ -1,8 +1,23 @@
 #pragma once
 #include <iostream>
 #include <vector>
+
+auto make_id_generator() {
+	static int count = 0;
+	return [&]() {
+		return ++count;
+		};
+}
+auto generate_id = make_id_generator();
+
+using PetId = int;
+
 class Pet {
 private:
+	std::vector<PetId> enemies;
+	std::vector<PetId> friends;
+
+	PetId id = generate_id();
 public:
 	virtual void feed(int hours, int minutes) = 0;
 	virtual void sleep(int hours, int minutes) = 0;
@@ -13,10 +28,8 @@ public:
 		std::vector<Pet> enemies;
 		enemies.push_back(pet);
 	}
-	virtual void add_friend(const Pet& friends, Pet pet, int num) {
-		//Задание N1
-		std::vector<Pet> friendss;
-		friendss.push_back(pet);
+	virtual void add_friend(const Pet& _friend) {
+		friends.push_back(_friend.id);
 	}
 	virtual void Test_works(int num, int vector1) {
 		std::vector<Pet> vector;
@@ -29,7 +42,7 @@ public:
 		}
 	}
 	//Задание N3
-	bool find_num(std::vector<int> & const num, int num_find) {
+	bool find_num(std::vector<int>& const num, int num_find) {
 		for (auto& number : num) {
 			if (number == num_find) {
 				return true;
