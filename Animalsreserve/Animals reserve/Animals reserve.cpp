b3bf94032da7535b12d80fb462b402f4dbcd5292
semaxik <semaxik@outlook.com>
2025-06-08 09:49:03 +0300

    tasks 2 and 3 have been corrected

--- Animalsreserve/Animals reserve/Animals reserve.cpp
+++ Animalsreserve/Animals reserve/Animals reserve.cpp
@@ -2,22 +2,33 @@
 #include <iostream>
 #include "dog.hpp"
 #include "cat.hpp"
+#include "Pet.hpp"
+void Test_works(int num, int vector1) {
+	std::vector<Pet> vector;
+	vector1[];
+	//Задание N2
+	for (auto& number : vector)
+	for (int i = 0; i < vector; ++i) {
+		std::cin >> vector1{ i } >> std::endl;
+		std::cin >> vector >> std::endl;
+		std::cout << vector << std::endl;
+	}
+	//Задание N3
+	vector[1]{ 3 };
+	vector.find_num(3);
+	if (vector.find_num(3) == true) {
+		std::cout << "Найдено";
+	}
+}
+//Задание N3
+bool find_num(std::vector<int>& const num, int num_find) {
+	for (auto& number : num) {
+		if (number == num_find) {
+			return true;
+		}
+	}
+	return false;
+}
 int main() {
-	Dog bobik("bobik");
-	Dog shah("shah");
-	Dog barbos("barbos");
-	bobik.add_enemy(shah);
-	if (bobik.has_enemy(shah)) {
-		std::cout << "гулять нельзя, враг!!!"<<std::endl;
-	}
-	else {
-		std::cout << "гулять можно" << std::endl;
-	};
 
-	if (bobik.has_enemy(barbos)) {
-		std::cout << "гулять нельзя, враг!!!" << std::endl;
-	}
-	else {
-		std::cout << "гулять можно" << std::endl;
-	};
 }
--- Animalsreserve/Animals reserve/Pet.hpp
+++ Animalsreserve/Animals reserve/Pet.hpp
@@ -22,32 +22,11 @@ public:
 	virtual void feed(int hours, int minutes) = 0;
 	virtual void sleep(int hours, int minutes) = 0;
 	virtual void walk(int hours, int minutes) = 0;
-	virtual void friends(char friends, char enemyes) = 0;
 	virtual void add_enemy(const Pet& enemy, Pet pet, int num) {
 		//Задание N1
 		std::vector<Pet> enemies;
-		enemies.push_back(pet);
+		enemies.push_back(enemy.id);
 	}
-	virtual void add_friend(const Pet& _friend) {
+	void add_friend(const Pet& _friend) {
+		std::vector<Pet> friends ;
 		friends.push_back(_friend.id);
-	}
-	virtual void Test_works(int num, int vector1) {
-		std::vector<Pet> vector;
-		vector1[];
-		//Задание N2
-		for (auto& number : vector) {
-			std::cin >> vector1{ i } >> std::endl;
-			std::cin >> vector >> std::endl;
-			std::cout << vector << std::endl;
-		}
-	}
-	//Задание N3
-	bool find_num(std::vector<int>& const num, int num_find) {
-		for (auto& number : num) {
-			if (number == num_find) {
-				return true;
-			}
-		}
-		return false;
-	}
-};
