618e4d2820736899cbe05f10bd90245fc1fca82d
semaxik <semaxik@outlook.com>
2025-06-14 14:46:35 +0300

    finally... Homework completed!

--- Animalsreserve/Animals reserve/Animals reserve.cpp
+++ Animalsreserve/Animals reserve/Animals reserve.cpp
@@ -19,33 +19,49 @@ void Test_work_2() {
 		std::cout << numbers[i] << std::endl;
 	}
 }
-void Test_work_3() {
-	std::vector<int> numbers;
-	int number_count;
-	int buffer;
-	int what_to_find;
-	
-	std::cin >> number_count;
-	for (int i = 0; i < number_count; ++i) {
-		std::cin >> buffer;
-		numbers.push_back(buffer);
+//void Test_work_3() {
+//	std::vector<int> numbers;
+//	int number_count;
+//	int buffer;
+//	int what_to_find;
+//	
+//	std::cin >> number_count;
+//	for (int i = 0; i < number_count; ++i) {
+//		std::cin >> buffer;
+//		numbers.push_back(buffer);
+//	}
+//	std::cin >> what_to_find;
+//	if (find_number_in_vector(numbers, what_to_find)) {
+//			std::cout << "Найдено";
+//	}
+//	else {
+//			std::cout << "Не найдено";
+//	}
+//}
+//bool find_number_in_vector(std::vector<int> & const numbers, int number_to_find) {
+//	for (auto& number : numbers) {
+//		if (number == number_to_find) {
+//			return true;
+//		}
+//	}
+//	return false;
+//}
+bool find_pair_of_numbers_in_vector(std::vector<int> numbers, int number_to_find, int number1, int number2) {
+	std::cin >> number_to_find;
+	for (number1 = 0; number1 < numbers.size(); ++number1) {
+		
 	}
-	std::cin >> what_to_find;
-	if (find_number(numbers, what_to_find)) {
-			std::cout << "Найдено";
-	}
-	else {
-			std::cout << "Не найдено";
-	}
-}
-bool find_number(std::vector<int> & const num, int num_find) {
-	for (auto& number : num) {
-		if (number == num_find) {
-			return true;
+	for (int i = 0; i < numbers.size(); ++i) {
+		if (number1 + numbers[i] == number_to_find) {
+			std::cout << number1, '+', numbers[i];
 		}
 	}
-	return false;
-}
+	return true;
+};
 int main() {
-
+	std::vector<int> numbers;
+	int number_to_find;
+	int number1;
+	int number2;
+	find_pair_of_numbers_in_vector(numbers, number_to_find, number1, number2);
 }
--- Animalsreserve/Animals reserve/cat.hpp
+++ Animalsreserve/Animals reserve/cat.hpp
@@ -3,63 +3,63 @@
 #include "Pet.hpp"
 class Cat : public Pet {
 public:
-    void feed (int hours, int minutes) override {
-        std::cout << "Кошка покормлена в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
-    }
+	void feed(int hours, int minutes) override {
+		std::cout << "Кошка покормлена в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
+	}
 
-    void sleep (int hours, int minutes) override {
-        std::cout << "Кошка спит с " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
-    }
+	void sleep(int hours, int minutes) override {
+		std::cout << "Кошка спит с " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
+	}
 
-    void walk(int hours, int minutes) override {
-        std::cout << "Кошку надо выгулять в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
-    }
-    void friends(const char friends, char enemyes, const char murzik, const char lio, const char vasya) override {
-		
-		Cat murzik("murzik");
-		Cat lio("lio");
-		Cat vasya("vasya");
+	void walk(int hours, int minutes) override {
+		std::cout << "Кошку надо выгулять в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
+	}
+	//  void friends(const char friends, char enemyes, const char murzik, const char lio, const char vasya) override {
+		  //
+		  //Cat murzik("murzik");
+		  //Cat lio("lio");
+		  //Cat vasya("vasya");
 
-		Cat murzik.add_enemy(lio);
-		if (Cat murzik.has_enemy(lio)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
+		  //murzik.add_enemy(lio);
+		  //if (murzik.has_enemy(lio)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
 
-		if (Cat murzik.has_enemy(vasya)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
-		Cat vasya.add_enemy(murzik);
-		if (Cat vasya.has_enemy(lio)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
+		  //if (Cat murzik.has_enemy(vasya)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
+		  //Cat vasya.add_enemy(murzik);
+		  //if (Cat vasya.has_enemy(lio)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
 
-		if (Cat vasya.has_enemy(murzik)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
-		Cat lio.add_enemy(murzik);
-		if (Cat lio.has_enemy(murzik)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
+		  //if (Cat vasya.has_enemy(murzik)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
+		  //Cat lio.add_enemy(murzik);
+		  //if (Cat lio.has_enemy(murzik)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
 
-		if (Cat lio.has_enemy(vasya)) {
-			std::cout << "гулять нельзя, враг!!!" << std::endl;
-		}
-		else {
-			std::cout << "гулять можно" << std::endl;
-		};
-}
+		  //if (Cat lio.has_enemy(vasya)) {
+		  //	std::cout << "гулять нельзя, враг!!!" << std::endl;
+		  //}
+		  //else {
+		  //	std::cout << "гулять можно" << std::endl;
+		  //};
+};
