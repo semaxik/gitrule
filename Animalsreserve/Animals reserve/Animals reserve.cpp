2550a946702902607073d306e12a8c2f5f17c456
Alexandr Petrov <alexandr.petrov@innroad.com>
2025-06-15 20:14:51 +0300

    implemented two pointers pair search

--- Animalsreserve/Animals reserve/Animals reserve.cpp
+++ Animalsreserve/Animals reserve/Animals reserve.cpp
@@ -29,74 +29,77 @@ void test_work_2() {
 		std::cout << numbers[i] << std::endl;
 	}
 }
-<<<<<<< HEAD
-//void Test_work_3() {
-//	std::vector<int> numbers;
-//	int number_count;
-//	int buffer;
-//	int what_to_find;
-//	
-//	std::cin >> number_count;
-//	for (int i = 0; i < number_count; ++i) {
-//		std::cin >> buffer;
-//		numbers.push_back(buffer);
-//	}
-//	std::cin >> what_to_find;
-//	if (find_number_in_vector(numbers, what_to_find)) {
-//			std::cout << "Найдено";
-//	}
-//	else {
-//			std::cout << "Не найдено";
-//	}
-//}
-//bool find_number_in_vector(std::vector<int> & const numbers, int number_to_find) {
-//	for (auto& number : numbers) {
-//		if (number == number_to_find) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-bool find_pair_of_numbers_in_vector(std::vector<int> numbers, int number_to_find, int number1, int number2) {
-	std::cin >> number_to_find;
-	for (number1 = 0; number1 < numbers.size(); ++number1) {
-		
-	}
-	for (int i = 0; i < numbers.size(); ++i) {
-		if (number1 + numbers[i] == number_to_find) {
-			std::cout << number1, '+', numbers[i];
+
+bool find_pair_of_numbers_in_vector_two_pointers(std::vector<int> numbers, int number_to_find) {
+	int left = 0;
+	int right = numbers.size() - 1;
+	while (left < right) {
+		int left_number = numbers[left];
+		int right_number = numbers[right];
+		if (left_number + right_number == number_to_find) {
+			std::cout << left_number << " + " << right_number << std::endl;
+			return true;
+		}
+		if (left_number + right_number > number_to_find) {
+			right--;
+		}
+		else {
+			left++;
 		}
 	}
-	return true;
+	return false;
+}
+
+bool find_pair_of_numbers_in_vector_naive(std::vector<int> numbers, int number_to_find) {
+	for (int k = 0; k < numbers.size(); ++k) {
+		for (int i = 0; i < numbers.size(); ++i) {
+			int left_number = numbers[k];
+			int right_number = numbers[i];
+			if (left_number + right_number == number_to_find && left_number != right_number) {
+				std::cout << left_number << " + " << right_number << std::endl;
+				return true;
+			}
+		}
+	}
+
+	return false;
 };
-int main() {
-	std::vector<int> numbers;
-	int number_to_find;
-	int number1;
-	int number2;
-	find_pair_of_numbers_in_vector(numbers, number_to_find, number1, number2);
-=======
 
-void test_work_3() {
-	std::vector<int> numbers;
-	int number_count;
-	int buffer;
-	int what_to_find;
+int sum(int a, int b) {
+	return a + b;
+}
 
-	std::cin >> number_count;
-	for (int i = 0; i < number_count; ++i) {
-		std::cin >> buffer;
-		numbers.push_back(buffer);
-	}
-	std::cin >> what_to_find;
-	if (find_number(numbers, what_to_find)) {
-		std::cout << "Найдено";
-	}
-	else {
-		std::cout << "Не найдено";
-	}
+void print(int number) {
+	std::cout << number << std::endl;
+}
+void print(double number) { // перегруженная функция для чисел с плавающей точкой
+	std::cout << number << std::endl;
+}
+void print(unsigned long number) { // перегруженная функция для беззнаковых "длинных" целых
+	std::cout << number << std::endl;
+}
+void print(std::string string) { //перегруженная функция для строк
+	std::cout << string << std::endl;
 }
 
 int main() {
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
+	double a_floating = 1.5;
+	int a_number = 10;
+	unsigned long a_long_number = 0xFFFFFFFF;
+	std::string a_string = "10";
+
+	print(a_number);
+	print(a_floating);
+	print(a_long_number);
+	print(a_string);
+
+	std::vector<int> numbers = { 1,2,3,4 };
+	int number_to_find;
+
+	std::cout << "number to find a pair for: "; //перед тем как что-то запрашивать надо дать знать пользователю
+	//что от него требуется
+	std::cin >> number_to_find;
+
+	find_pair_of_numbers_in_vector_two_pointers(numbers, number_to_find);
+	find_pair_of_numbers_in_vector_naive(numbers, number_to_find);
 }
--- Animalsreserve/Animals reserve/cat.hpp
+++ Animalsreserve/Animals reserve/cat.hpp
@@ -3,12 +3,9 @@
 #include "Pet.hpp"
 class Cat : public Pet {
 public:
-<<<<<<< HEAD
-=======
 	Cat(std::string name)
 		:Pet(name) {
 	}
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
 	void feed(int hours, int minutes) override {
 		std::cout << " ошка покормлена в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
 	}
@@ -16,62 +13,8 @@ public:
 	void sleep(int hours, int minutes) override {
 		std::cout << " ошка спит с " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
 	}
-<<<<<<< HEAD
 
 	void walk(int hours, int minutes) override {
 		std::cout << " ошку надо выгул€ть в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
 	}
-	//  void friends(const char friends, char enemyes, const char murzik, const char lio, const char vasya) override {
-		  //
-		  //Cat murzik("murzik");
-		  //Cat lio("lio");
-		  //Cat vasya("vasya");
-
-		  //murzik.add_enemy(lio);
-		  //if (murzik.has_enemy(lio)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-
-		  //if (Cat murzik.has_enemy(vasya)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-		  //Cat vasya.add_enemy(murzik);
-		  //if (Cat vasya.has_enemy(lio)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-
-		  //if (Cat vasya.has_enemy(murzik)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-		  //Cat lio.add_enemy(murzik);
-		  //if (Cat lio.has_enemy(murzik)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-
-		  //if (Cat lio.has_enemy(vasya)) {
-		  //	std::cout << "гул€ть нельз€, враг!!!" << std::endl;
-		  //}
-		  //else {
-		  //	std::cout << "гул€ть можно" << std::endl;
-		  //};
-=======
-	void walk(int hours, int minutes) override {
-		std::cout << " ошку надо выгул€ть в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
-	}
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
 };
