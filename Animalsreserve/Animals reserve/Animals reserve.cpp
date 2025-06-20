9ce2db2b16738128869611a99e25b4591b496c20
semaxik <semaxik@outlook.com>
2025-06-20 20:32:22 +0300

    nothing happend

--- Animalsreserve/Animals reserve/Animals reserve.cpp
+++ Animalsreserve/Animals reserve/Animals reserve.cpp
@@ -13,23 +13,22 @@ bool find_number(std::vector<int>& const num, int num_find) {
 	return false;
 }
 
-void test_work_2() {
-	std::vector<int> numbers;
-	int number_count;
-	int buffer;
-
-	std::cin >> number_count;
-
-	for (int i = 0; i < number_count; ++i) {
-		std::cin >> buffer;
-		numbers.push_back(buffer);
-	}
-
-	for (int i = 0; i < numbers.size(); ++i) {
-		std::cout << numbers[i] << std::endl;
-	}
-}
-<<<<<<< HEAD
+//void test_work_2() {
+//	std::vector<int> numbers;
+//	int number_count;
+//	int buffer;
+//
+//	std::cin >> number_count;
+//
+//	for (int i = 0; i < number_count; ++i) {
+//		std::cin >> buffer;
+//		numbers.push_back(buffer);
+//	}
+//
+//	for (int i = 0; i < numbers.size(); ++i) {
+//		std::cout << numbers[i] << std::endl;
+//	}
+//}
 //void Test_work_3() {
 //	std::vector<int> numbers;
 //	int number_count;
@@ -60,14 +59,14 @@ void test_work_2() {
 bool find_pair_of_numbers_in_vector(std::vector<int> numbers, int number_to_find, int number1, int number2) {
 	std::cin >> number_to_find;
 	for (number1 = 0; number1 < numbers.size(); ++number1) {
-		
-	}
-	for (int i = 0; i < numbers.size(); ++i) {
-		if (number1 + numbers[i] == number_to_find) {
-			std::cout << number1, '+', numbers[i];
+		for (int i = 0; i < numbers.size(); ++i) {
+			if (number1 + numbers[i] == number_to_find) {
+				std::cout << number1, '+', numbers[i];
+				return true;
+			}
 		}
 	}
-	return true;
+	return false;
 };
 int main() {
 	std::vector<int> numbers;
@@ -75,28 +74,4 @@ int main() {
 	int number1;
 	int number2;
 	find_pair_of_numbers_in_vector(numbers, number_to_find, number1, number2);
-=======
-
-void test_work_3() {
-	std::vector<int> numbers;
-	int number_count;
-	int buffer;
-	int what_to_find;
-
-	std::cin >> number_count;
-	for (int i = 0; i < number_count; ++i) {
-		std::cin >> buffer;
-		numbers.push_back(buffer);
-	}
-	std::cin >> what_to_find;
-	if (find_number(numbers, what_to_find)) {
-		std::cout << "Найдено";
-	}
-	else {
-		std::cout << "Не найдено";
-	}
-}
-
-int main() {
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
 }
--- Animalsreserve/Animals reserve/cat.hpp
+++ Animalsreserve/Animals reserve/cat.hpp
@@ -3,12 +3,9 @@
 #include "Pet.hpp"
 class Cat : public Pet {
 public:
-<<<<<<< HEAD
-=======
 	Cat(std::string name)
 		:Pet(name) {
 	}
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
 	void feed(int hours, int minutes) override {
 		std::cout << "Кошка покормлена в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
 	}
@@ -16,7 +13,6 @@ public:
 	void sleep(int hours, int minutes) override {
 		std::cout << "Кошка спит с " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
 	}
-<<<<<<< HEAD
 
 	void walk(int hours, int minutes) override {
 		std::cout << "Кошку надо выгулять в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
@@ -69,9 +65,4 @@ public:
 		  //else {
 		  //	std::cout << "гулять можно" << std::endl;
 		  //};
-=======
-	void walk(int hours, int minutes) override {
-		std::cout << "Кошку надо выгулять в " << hours << ":" << (minutes < 10 ? "0" : "") << minutes;
-	}
->>>>>>> f7f0223d222880f76504b65fe9792015d034ca7d
 };
